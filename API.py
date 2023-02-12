42c07ab7be33528a1769f675edbffc95c20119de
ReactAppPrice <123896748+ReactAppPrice@users.noreply.github.com>
2023-02-12 16:54:29 +0500

    Update API.py

--- API.py
+++ API.py
@@ -12,6 +12,7 @@ def query_example():
     return resp.json()
 
 
+
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, port=5000)
